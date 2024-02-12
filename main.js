c57013ba1294da1e359d4dbcaaaca9c66fd5a1ba
Mahi <ahnafraismahi@gmail.com>
2024-02-12 11:28:41 +0600

    animated background to new branch

--- main.js
+++ main.js
@@ -6,3 +6,21 @@ document.addEventListener('DOMContentLoaded', function () {
         loop:true,
     });
 });
+function lines() {
+    let sizeW = Math.random() * 12
+    let duration = Math.random() * 3
+    let e = document.createElement("div")
+    e.setAttribute("class", "circle")
+    document.body.appendChild(e)
+    e.style.width = 2 + sizeW + "px"
+    e.style.left = Math.random() * innerWidth + "px"
+    e.style.animationDuration = 2 + duration + "s"
+
+    setTimeout(() => {
+        document.body.removeChild(e)
+    }, 5000)
+}
+
+setInterval(() => {
+    lines()
+}, 100)
